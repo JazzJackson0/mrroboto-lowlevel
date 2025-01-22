ac759a43bfd6113f1bf590f21390666c29ad7b2f
Jazz Jackson <jazzjacksonhomebase@gmail.com>
2025-01-21 22:26:47 -0500

    Moved pico initialization code from tasks.c to main

--- src/mrroboto_lowlevel.c
+++ src/mrroboto_lowlevel.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include "../include/tasks.h"
+// #include "boards/pico_w.h"
+// #include "pico/stdlib.h"
 
 
 int main(int argc, char *argv) {
 
-    
+    stdio_init_all();
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed");
+        return -1;
+    }
+
     start_tasks();
 
     while (1) {
         tight_loop_contents();
     }
 
+    cyw43_arch_deinit();
     return 0;
 }
 
