0274b1dda3df57284d317f7f96497e6ae9d22282
Jazz Jackson <jazzjackson049@gmail.com>
2025-11-12 22:14:34 -0500

    Update parameter name. Change pwm function call order.

--- src/motor.c
+++ src/motor.c
@@ -1,18 +1,18 @@
 #include "../include/motor.h"
 
-void initMotor(struct motor_data * motor, uint8_t pin, uint8_t dir_pin_1, uint8_t dir_pin_2) {
+void initMotor(struct motor_data * motor, uint8_t pwm_pin, uint8_t dir_pin_1, uint8_t dir_pin_2) {
 
     // PWM Motor Output Setup
     motor->direction_pin_1 = dir_pin_1;
     motor->direction_pin_2 = dir_pin_2;
     motor->direction = 0;
-    motor->slice_num = pwmSetup(pin);
+    motor->slice_num = pwmSetup(pwm_pin);
 }
 
 void motorSpeedOut(struct motor_data * left_motor, struct motor_data * right_motor) {
     
-    pwmUpdateDutyCycle(right_motor->slice_num, right_motor->duty_cycle_percent);
-    pwmUpdateDutyCycle(left_motor->slice_num, left_motor->duty_cycle_percent);
+    pwmUpdateDutyCycle(left_motor->slice_num, PWM_CHAN_A, left_motor->duty_cycle_percent);
+    pwmUpdateDutyCycle(right_motor->slice_num, PWM_CHAN_B, right_motor->duty_cycle_percent);
 }
 
 void motorDirectionOut(struct motor_data * left_motor, struct motor_data * right_motor) {
