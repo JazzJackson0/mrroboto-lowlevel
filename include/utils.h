5aeb13dc953c0f90472cf3d741e603247c04fa00
Jazz Jackson <jazzjackson049@gmail.com>
2025-12-01 15:35:53 -0500

    Add ring buffer

--- include/utils.h
+++ include/utils.h
@@ -5,6 +5,12 @@
 #include "../CMSIS-DSP-main/Include/arm_math.h"
 
 
+struct ring_buffer {
+    uint8_t * buffer;
+    size_t size;
+    uint8_t producer_idx;
+    uint8_t consumer_idx;
+};
 
 
 /**
@@ -17,8 +23,26 @@
  */
 void enqueue(float32_t queue[], int queue_size, float32_t val, int idx);
 
+/**
+ * @brief
+ * @param buffer
+ * @param raw_buffer
+ * @param buffer_size
+ */
+void ringBufferInit(struct ring_buffer * buffer, uint8_t * raw_buffer, size_t buffer_size);
 
+/**
+ * @brief
+ * @param buffer
+ * @param value
+ */
+void ringBufferProduceUint8(struct ring_buffer * buffer, uint8_t value);
 
+/**
+ * @brief
+ * @param buffer
+ */
+uint8_t ringBufferConsumeUint8(struct ring_buffer * buffer);
 
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -15,3 +15,46 @@ void enqueue(float32_t queue[], int queue_size, float32_t val, int idx) {
     // Enqueue new value
     queue[idx] = val;
 }
+
+void ringBufferInit(struct ring_buffer * buffer, uint8_t * raw_buffer, size_t buffer_size) {
+
+    buffer->buffer = raw_buffer; 
+    buffer->size = buffer_size;
+
+    for (int i = 0; i < buffer_size; i++) {
+        buffer->buffer[i] = 0;
+    }
+    buffer->producer_idx = 0;
+    buffer->consumer_idx = 0;
+}
+
+
+void ringBufferProduceUint8(struct ring_buffer * buffer, uint8_t value) {
+
+    buffer->buffer[buffer->producer_idx++] = value;
+
+    // Wrap around if needed
+    if (buffer->producer_idx >= buffer->size) {
+        buffer->producer_idx = 0;
+    }
+}
+
+
+uint8_t ringBufferConsumeUint8(struct ring_buffer * buffer) {
+
+    // If consumer has caught up with producer
+    if (buffer->consumer_idx == buffer->producer_idx) {
+        // Return last valid data
+        return buffer->buffer[buffer->consumer_idx - 1];
+    }
+
+
+    uint8_t value = buffer->buffer[buffer->consumer_idx++];
+    
+    // Wrap around if needed
+    if (buffer->consumer_idx >= buffer->size) {
+        buffer->consumer_idx = 0;
+    }
+
+    return value;
+}
